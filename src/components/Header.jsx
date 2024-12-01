873ab3a341758e4b5da93a4650c687dd534fea59
CompetentHeisenberg <competentheisenberg@gmail.com>
2024-12-01 11:13:29 +0200

    Test

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import header from "../css/main_header.module.css";
+
+function Header() {
+  return (
+    <header className={header.header}>
+      <div>
+        <p>hello</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/mpage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "../components/Header";
+import style from "../css/mpage.module.css";
+function mainpage() {
+  return (
+    <div>
+      <Head></Head>
+      <p>hello</p>
+    </div>
+  );
+}
+
+export default mainpage;
